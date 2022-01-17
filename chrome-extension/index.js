30ecf0f0fbb7ddeb40039898b28afb810e4d6899
risha-parveen <rishaparveen12@gmail.com>
2022-01-17 11:08:43 +0530

    updated

--- chrome-extension/index.js
+++ chrome-extension/index.js
@@ -1,17 +1,35 @@
 
-let myLeads=["www.a.com","www.b.com","www.c.com"]
+let myLeads=[]
 const inputBtn= document.getElementById("input-btn")
 const inputEl=document.getElementById("input-el")
 const ulEl=document.getElementById("ul-el")
 
 inputBtn.addEventListener("click",function(){
-    myLeads.push(inputEl.value)
-    console.log(myLeads)
+    
+    if(inputEl.value.trim().length==0){
+        alert("Please provide input")
+    }
+    else{
+        myLeads.push(inputEl.value)
+        renderLeads()
+    }
 })
 
-for(let i=0;i<myLeads.length;i++){
-    //ulEl.innerHTML+= "<li>" +myLeads[i]+ "</li>"
-    const li=document.createElement("li")
-    li.textContent= myLeads[i]
-    ulEl.append(li)
+function renderLeads(){
+    let listItems=""
+
+    for(let i=0;i<myLeads.length;i++){
+        listItems+= `
+            <li>
+                <a target='_blank' href="${myLeads[i]}">
+                    ${myLeads[i]}
+                </a>
+            </li>
+        `
+    }
+    ulEl.innerHTML=listItems
+    inputEl.value=""
 }
+
+
+
