d1f6b9a37112e59f55e1577d3f27aff90101c63d
risha-parveen <rishaparveen12@gmail.com>
2022-01-18 09:24:42 +0530

    updated

--- leads-tracker/index.js
+++ leads-tracker/index.js
@@ -3,51 +3,57 @@ let myLeads=[]
 const inputBtn= document.getElementById("input-btn")
 const inputEl=document.getElementById("input-el")
 const ulEl=document.getElementById("ul-el")
+const deleteBtn=document.getElementById("delete-btn")
 
 //localStorage.setItem("myName","www.risha.com")
 //console.log(localStorage.getItem("myName"))
 //localStorage.clear()
 
-let leadsFromLocalStorage=JSON.parse(localStorage.getItem("myLeads"))
+const leadsFromLocalStorage=JSON.parse(localStorage.getItem("myLeads"))
 
 if(leadsFromLocalStorage){
     myLeads=leadsFromLocalStorage
-    renderLeads()
+    render(myLeads)
 }
 
 inputBtn.addEventListener("click",function(){
-    saveLeads()
+    save(myLeads)
 })
 
 inputEl.addEventListener("keypress",function(e){
     if(e.key==='Enter'){
-        saveLeads()
+        save(myLeads)
     }   
 })
 
-function saveLeads(){
-    console.log("heloo")
+deleteBtn.addEventListener("dblclick",function(){
+    localStorage.clear()
+    myLeads=[]
+    render(myLeads)
+})
+
+function save(leads){
     if(inputEl.value.trim().length==0){
         alert("Please provide input")
     }
     else{
-        myLeads.push(inputEl.value) 
-        localStorage.setItem("myLeads",JSON.stringify(myLeads))      
-        renderLeads()
+        leads.push(inputEl.value) 
+        localStorage.setItem("myLeads",JSON.stringify(leads))      
+        render(leads)
     }
 }
 
 
 
-function renderLeads(){
+function render(leads){
     let listItems=""
 
-    for(let i=0;i<myLeads.length;i++){
+    for(let i=0;i<leads.length;i++){
         
         listItems+= `
             <li>
-                <a target='_blank' href="${myLeads[i]}">
-                    ${myLeads[i]}
+                <a target='_blank' href="${leads[i]}">
+                    ${leads[i]}
                 </a>
             </li>
         `
